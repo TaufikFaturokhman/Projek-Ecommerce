616f729fff613cd78f92281cf559df31fd20e7f9
Reckend <Spartancoc04@gmail.com>
2023-11-08 19:11:26 +0700

    feat-crud product variant

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -126,5 +126,125 @@ module.exports ={
             })
         }
     },
-    
+    addVariant : async (req,res) =>{
+        try{
+            const process = await product_variants.create({
+                data:{
+                    name : req.body.name,
+                    product_id : parseInt(req.body.product_id)
+                }
+            })
+            if (process){
+                return res.status(200).json({
+                    success: "success to updating data product variant",
+                })
+            }
+            return res.status(500).json({
+                success: false
+            })
+        }
+        catch (err){
+            console.error (`Error adding the data product variant : ${err}`)
+            return res.status(500).json({
+                error:`An error occured while adding the data product variant`
+            })
+        }
+    },
+    getVariant : async (req,res) =>{
+        try{
+            const data = product_variants.findMany({})
+            if (data){
+                return res.status(200).json({
+                    success : true,
+                    data : data
+                })
+            }
+            return res.status(500).json({
+                success : false
+            })
+        }
+        catch(err){
+            console.error (`Error getting the data product variant : ${err}`)
+            return res.status(500).json({
+                error:`An error occured while getting the data product variant`
+            })
+        }
+    },
+    getVariantById : async (req,res) =>{
+        try{
+            const id = parseInt(req.params.id)
+            const data = await product_variants.findFirst({
+                where:{
+                    id:id
+                }
+            })
+            if(data){
+                return res.status(200).json({
+                    success:true,
+                    data: data
+                })
+            }
+            return res.status(500).json({
+                success:false
+            })
+        }
+        catch(err){
+            console.error (`Error getting the data product variant by id : ${err}`)
+            return res.status(500).json({
+                error:`An error occured while getting the data product variant by id`
+            })
+        }
+    },
+    updateVariant : async (req,res) => {
+        try{
+            const id = parseInt(req.params.id)
+            const process = await product_variants.update({
+                where:{
+                    id:id
+                },
+                data:{
+                    name : req.body.name,
+                    product_id : parseInt(req.body.product_id)
+                }
+            })
+            if(process){
+                return res.status(200).json({
+                    succeess: "succes to updating the data Product Variant"
+                })
+            }
+            res.status(500).json({
+                success : false
+            })
+        }
+        catch (err){
+            console.error (`Error updating the data product variant: ${err}`)
+            return res.status(500).json({
+                error:`An error occured while updating the data product variant `
+            })
+        }
+    },
+    deleteVariant : async(req,res) =>{
+        try{
+            const id = parseInt(req.params.id)
+            const process = await product_variants.delete({
+                where:{
+                    id:id
+                }
+            })
+            if(process){
+                return res.status(200).json({
+                    success: "success deleting the product variant"
+                })
+            }
+            return res.status(500).json({
+                success:false
+            })
+        }
+        catch (err){
+            console.error (`Error deleting the data product variant: ${err}`)
+            return res.status(500).json({
+                error:`An error occured while deleting the data product variant `
+            })
+        }
+    }
 }
--- routers/product.routers.js
+++ routers/product.routers.js
@@ -8,4 +8,10 @@ router.get('/getProductById/:id', controller.getProductId)
 router.put('/updateProduct', controller.updateProduct)
 router.delete('/deleteProduct', controller.deleteProduct)
 
+router .post('/addVariant',controller.addVariant)
+router.get('/getVariant', controller.getVariant)
+router.get('/getVariantById/:id', controller.getVariantById)
+router.put('/updateVariant', controller.updateVariant)
+router.delete('/deleteVariant', controller.deleteVariant)
+
 module.exports = router
